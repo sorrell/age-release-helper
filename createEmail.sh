7cfd1e371b1b75e1d91541655f8ffc057cc5d003
Nick Sorrell <nick@cint.io>
2022-03-21 19:23:13 -0400

    Adding separate release/vote email templates

--- createEmail.sh
+++ createEmail.sh
@@ -7,10 +7,15 @@ set -e
 AGE_GIT_HASH=$(cat $AGE_COMMIT_FILE)
 AGE_REL_NOTES=$(cat "./releaseNotes.md")
 
-awk -v version="$AGE_VERSION" '{gsub("{{AGE_VERSION}}", version, $0); print}' emailTemplate.md |
-  awk -v rc="$AGE_RC" '{gsub("{{AGE_RC}}", rc, $0); print}' |
-  awk -v hash="$AGE_GIT_HASH" '{gsub("{{AGE_GIT_HASH}}", hash, $0); print}' |
-  awk -v relmgr="$RELEASE_MGR" '{gsub("{{RELEASE_MGR}}", relmgr, $0); print}' | 
-  awk -v gpgsig="$GPG_SIG" '{gsub("{{GPG_SIG}}", gpgsig, $0); print}' |
-  awk -v relnotes="$AGE_REL_NOTES" '{gsub("{{RELEASE_NOTES}}", relnotes, $0); print}' \
-  > "$AGE_EMAIL_FILE"
+function create_emaildoc_from_template {
+  awk -v version="$AGE_VERSION" '{gsub("{{AGE_VERSION}}", version, $0); print}' $1 |
+    awk -v rc="$AGE_RC" '{gsub("{{AGE_RC}}", rc, $0); print}' |
+    awk -v hash="$AGE_GIT_HASH" '{gsub("{{AGE_GIT_HASH}}", hash, $0); print}' |
+    awk -v relmgr="$RELEASE_MGR" '{gsub("{{RELEASE_MGR}}", relmgr, $0); print}' | 
+    awk -v gpgsig="$GPG_SIG" '{gsub("{{GPG_SIG}}", gpgsig, $0); print}' |
+    awk -v relnotes="$AGE_REL_NOTES" '{gsub("{{RELEASE_NOTES}}", relnotes, $0); print}' \
+    > "$2_$AGE_EMAIL_FILE"
+}
+
+create_emaildoc_from_template "emailTemplate_discuss.md" "discuss"
+create_emaildoc_from_template "emailTemplate_vote.md" "vote"
---
+++ emailTemplate_discuss.md
@@ -0,0 +1,39 @@
+Dear Apache Community,
+ 
+We would like to discuss the Apache AGE release {{AGE_VERSION}} that we have been working towards.
+ 
+To learn more about Apache AGE, please see https://age.apache.org/
+ 
+Functionality included and addressed in this release:
+ 
+{{RELEASE_NOTES}}
+
+*****************************************************************
+ 
+The git tag to be discussed and voted upon:
+https://github.com/apache/incubator-age/releases/tag/v{{AGE_VERSION}}-{{AGE_RC}}
+ 
+The git commit hash:
+      commit {{AGE_GIT_HASH}}
+ 
+The release files for {{AGE_VERSION}}, can be found at:
+ https://dist.apache.org/repos/dist/dev/incubator/age/{{AGE_VERSION}}.{{AGE_RC}}/
+ 
+Signatures used for AGE RCs can be found in this file:
+https://downloads.apache.org/incubator/age/KEYS 
+ 
+The fingerprint of key to sign release artifacts:
+{{GPG_SIG}}
+ 
+For information about the contents of this release, see:
+https://github.com/apache/incubator-age/releases/tag/v{{AGE_VERSION}}-{{AGE_RC}}
+ 
+*****************************************************************
+
+Please share your thoughts and feedback and let us know if we can call for a vote.
+
+Thank you for your time.
+
+
+Best Regards,
+{{RELEASE_MGR}}
